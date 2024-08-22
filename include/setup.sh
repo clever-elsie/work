47bdbd064a5a7642f6bf34c9dd7aff03bdc351d9
CleverElsie <yubisiba@gmail.com>
2024-08-23 02:38:52 +0900

    revise include/setup.sh. append symbolic link to /dev/shm and git clone lib_Clever_Elsie

--- include/setup.sh
+++ include/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo echo '/dev/shm/ /tmp tmpfs defaults,size=1024m 0 0' >> /etc/fstab
 git clone https://github.com/atcoder/ac-library
+git clone https://github.com/clever-elsie/lib_Clever_Elsie
 g++ macro.cpp -o macro.out -O2 -std=gnu++2a
 g++ new.cpp -o new.out -O2 -std=gnu++2a
 g++ local.h -o local.h.gch -std=gnu++2b -I . -I ./ac-library
+ln -s /dev/shm ../shm
