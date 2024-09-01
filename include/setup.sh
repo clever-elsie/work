b0c81acc4d41ed08330962088985b4cf2c3e0714
CleverElsie <yubisiba@gmail.com>
2024-09-02 01:43:54 +0900

    setup adding sample runner compile

--- include/setup.sh
+++ include/setup.sh
@@ -5,4 +5,6 @@ git clone https://github.com/clever-elsie/lib_Clever_Elsie ../lib_Clever_Elsie
 g++ macro.cpp -o macro.out -O2 -std=gnu++2a
 g++ new.cpp -o new.out -O2 -std=gnu++2a
 g++ local.h -o local.h.gch -std=gnu++2b -I . -I ./ac-library
+g++ sample_in.cpp -o sample_in.out -O2 -std=gnu++2b
+g++ sample_test.cpp -o sample_test.out -O2 -std=gnu++2b
 ln -s /dev/shm ../shm
