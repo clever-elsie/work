4f95072c54ee3802415a0b849498cad80797ea52
CleverElsie <yubisiba@gmail.com>
2025-04-24 21:36:14 +0900

    fix indent from 4spaces to 2spaces at template.cpp

--- include/tools/resource/template.cpp
+++ include/tools/resource/template.cpp
@@ -2,11 +2,11 @@
 #include <misc/alias.hpp>
 
 void slv(){
-    
+  
 }
 
 signed main(){
-    cin.tie(0)->sync_with_stdio(0);
-    cout<<fixed<<setprecision(15);
-    slv();
+  cin.tie(0)->sync_with_stdio(0);
+  cout<<fixed<<setprecision(15);
+  slv();
 }
